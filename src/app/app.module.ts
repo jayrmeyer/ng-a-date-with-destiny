4ef4e9b6e0b8b191baea3cc3fe574025a9c7b90b
Meyer <jason.r.meyer@accenture.com>
2018-01-30 09:31:27 -0600

    New PC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   MatAutocompleteModule,
@@ -40,16 +39,10 @@ import { AppComponent } from './app.component';
 import { CurrentNightfallComponent } from './current-nightfall/current-nightfall.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BungieService } from './services/bungie.service';
-=======
-
-
-import { AppComponent } from './app.component';
->>>>>>> chore: initial commit from @angular/cli
 
 
 @NgModule({
   declarations: [
-<<<<<<< HEAD
     AppComponent,
     CurrentNightfallComponent
   ],
@@ -90,14 +83,6 @@ import { AppComponent } from './app.component';
     MatStepperModule,
   ],
   providers: [BungieService],
-=======
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
->>>>>>> chore: initial commit from @angular/cli
   bootstrap: [AppComponent]
 })
 export class AppModule { }
